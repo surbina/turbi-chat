160091dc4854d79913375642ba6f06a21a2f36a3
Sebastian Urbina <sebita.urbina@gmail.com>
2019-04-02 22:19:21 -0300

    refactor(chat/message-form): use is.js instead of detect-mobile-browser to check if the app is runni

--- src/chat/components/MessageForm/Presentational.js
+++ src/chat/components/MessageForm/Presentational.js
@@ -5,11 +5,9 @@ import InputBase from '@material-ui/core/InputBase';
 import IconButton from '@material-ui/core/IconButton';
 import SendRoundedIcon from '@material-ui/icons/SendRounded';
 import { withStyles } from '@material-ui/core/styles';
-import getSmartPhoneDetector from 'detect-mobile-browser/detect-browser';
+import is from 'is_js';
 import { SUBMIT_BUTTON_LABEL } from './strings';
 import styles from './styles';
-
-const smartPhone = getSmartPhoneDetector(false);
 
 function MessageForm({ onSubmit, onChange, classes }) {
   const [message, setMessage] = useState('');
@@ -27,7 +25,7 @@ function MessageForm({ onSubmit, onChange, classes }) {
   };
 
   const handleKeyDown = (event) => {
-    if (!smartPhone.isAny() && event.keyCode === 13 && event.shiftKey === false) {
+    if (is.desktop() && event.keyCode === 13 && event.shiftKey === false) {
       handleSubmit(event);
     }
   };
